6f417c840d2c8e5a38b357419eeb1918f979c425
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-09-07 00:25:35 +0530

    initial commit - setup scss

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest, watch, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+
+function buildDistStyles() {
+  return src("scss/styles.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["scss/styles.scss"], buildDistStyles);
+}
+
+exports.default = series(buildDistStyles, watchTask);
