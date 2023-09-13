d4a838cd188f407843af8e4e03dd4a8805995f1f
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-09-13 12:33:05 +0530

    re-arrange folder structure to deploy from /public folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildDistStyles() {
-  return src("scss/styles.scss").pipe(sass()).pipe(dest("css"));
+  return src("src/scss/styles.scss").pipe(sass()).pipe(dest("public/css"));
 }
 
 function watchTask() {
-  watch(["scss/**/*.scss"], buildDistStyles);
+  watch(["src/scss/**/*.scss"], buildDistStyles);
 }
 
 exports.default = series(buildDistStyles, watchTask);
